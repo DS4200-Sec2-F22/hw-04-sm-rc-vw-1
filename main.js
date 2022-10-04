b60163ec61fb2365d9d54c4d81cc0e8a2abb8393
Vivian Weigel <weigel.v@northeastern.edu>
2022-10-04 11:52:54 -0400

    Clicking Functions Added

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// NEED TO ADD BORDER TO CIRCLES WHEN CLICKED
+// ADD BORDER TO CIRCLES WHEN CLICKED
+// gets all circles
+var circles = document.getElementsByTagName("circle");
+
+// goes through circles and adds event handler to each
+for (var i = 0; i < circles.length; i++) {
+    document.getElementsByTagName("circle")[i].addEventListener("click", function() {
+    // adding border
+    this.classList.toggle("borderAdd");
+    
+    // displaying coordinate
+    var x = (this.getAttribute('cx')) / 40;
+    var y = ((this.getAttribute('cy'))- 370) / -40;
+    var pointdisplay = document.getElementById('pointClick');
+    var pointtext = document.getElementById('point');
+    pointtext.innerHTML = ' (' + x + ', ' + y + ')';
+    pointdisplay.style.display = 'block';
+    pointtext.style.display = 'block';
+  });
+}
