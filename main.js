4e14a135fea4585526937455e1703915ee8ee98b
rachelcassway <105314113+rachelcassway@users.noreply.github.com>
2022-10-04 13:34:36 -0400

    drop down menus

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// ADD BORDER TO CIRCLES WHEN CLICKED
 // gets all circles
 var circles = document.getElementsByTagName("circle");
 
@@ -10,11 +9,28 @@ for (var i = 0; i < circles.length; i++) {
     
     // displaying coordinate
     var x = (this.getAttribute('cx')) / 40;
-    var y = ((this.getAttribute('cy'))- 370) / -40;
+    var y = ((this.getAttribute('cy')) - 375) / -40;
     var pointdisplay = document.getElementById('pointClick');
     var pointtext = document.getElementById('point');
     pointtext.innerHTML = ' (' + x + ', ' + y + ')';
     pointdisplay.style.display = 'block';
     pointtext.style.display = 'block';
   });
+}
+
+// get new point
+function getPoint() {
+
+    var x_coord = document.getElementById("x_coord");
+    var y_coord = document.getElementById("y_coord");
+
+    var cx = x_coord / 40;
+    var cy = (y_coord - 375) / - 40;
+
+    document.getElementById("demo").innerHTML = x_coord.options[x_coord.selectedIndex].text;
+    document.getElementById("demo2").innerHTML = y_coord.options[y_coord.selectedIndex].text;
+
+    document.getElementById("new_point").cx = cx;
+    // HOW CAN I MAKE CX AND CY WITH THE ABOVE POINTS
+
 }
