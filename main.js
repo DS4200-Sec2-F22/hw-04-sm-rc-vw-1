6ffe33cae2f9f11187aa155c28ed47dc9d78f61d
Vivian Weigel <weigel.v@northeastern.edu>
2022-10-06 14:15:53 -0400

    added comments

--- main.js
+++ main.js
@@ -7,33 +7,42 @@ for (var i = 0; i < circles.length; i++) {
     // adding border
     this.classList.toggle("borderAdd");
     
-    // displaying coordinate
+    // getting coordinate info and converting back 
     var x = (this.getAttribute('cx')) / 40;
     var y = ((this.getAttribute('cy')) - 375) / -40;
+
+    // saving part of html to display coordinate as variables
     var pointdisplay = document.getElementById('pointClick');
     var pointtext = document.getElementById('point');
+    // formatting coordinate and adding to html
     pointtext.innerHTML = ' (' + x + ', ' + y + ')';
+    // displaying the text of the coordinate and label
     pointdisplay.style.display = 'block';
     pointtext.style.display = 'block';
   });
 }
 
-// get new point
+// plots new point from drop down
 function getPoint() {
-
+	// saves drop down menus in variables
     var x_coord = document.getElementById("x_coord");
     var y_coord = document.getElementById("y_coord");
 
+    // extracts the option chosen from drop down
     x_coord = x_coord.options[x_coord.selectedIndex].text;
     y_coord = y_coord.options[y_coord.selectedIndex].text;
 
+    // scales selected coordinate to graph
     var cx = x_coord * 40;
     var cy = 375 - (y_coord * 40);
 
+    // plots new circle 
     var newP = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
     newP.setAttribute('cx', cx);
     newP.setAttribute('cy', cy);
     newP.setAttribute('r', 10);
+
+    //adds the click function from above to the new point
     newP.addEventListener("click", function clicker() {
     // adding border
     this.classList.toggle("borderAdd");
@@ -47,7 +56,8 @@ function getPoint() {
     pointdisplay.style.display = 'block';
     pointtext.style.display = 'block';
   });
-
+    
+    //appending new circle to the frame
     var svg = document.getElementById('frame');
     svg.appendChild(newP);
 
